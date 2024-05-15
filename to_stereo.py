eda8edb0caf48d59d2d207ea1e47adadcd396342
Evan Pierce <evan@mrthinger.com>
2024-05-15 12:54:23 +0000

    stereo batching

--- to_stereo.py
+++ to_stereo.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import ffmpeg
 from dataclasses import dataclass
@@ -6,59 +7,6 @@ from tqdm import tqdm
 from datetime import datetime
 
 from funcs import VideoInfo, get_video_info
-
-def load_video_buffer(filepath, video_info: VideoInfo):
-    video_buffer = np.zeros(
-        (video_info.num_frames, video_info.height, video_info.width, 3), dtype=np.uint8
-    )
-    process = (
-        ffmpeg.input(filepath, threads=16, thread_queue_size=8192)
-        .output("pipe:", format="rawvideo", pix_fmt="rgb24")
-        .run_async(pipe_stdout=True)
-    )
-    for i in range(video_info.num_frames):
-        in_bytes = process.stdout.read(video_info.width * video_info.height * 3)
-        if not in_bytes:
-            break
-        video_buffer[i] = np.frombuffer(in_bytes, np.uint8).reshape(
-            [video_info.height, video_info.width, 3]
-        )
-    process.wait()
-    return video_buffer
-
-
-def write_output_video(output_path, video_buffer: NDArray, video_info: VideoInfo, filepath):
-    in_modified = ffmpeg.input(
-        "pipe:",
-        format="rawvideo",
-        pix_fmt="rgb24",
-        s=f"{video_info.width}x{video_info.height}",
-        framerate=video_info.framerate,
-        thread_queue_size=8192,
-    )
-    in_original = ffmpeg.input(
-        filepath,
-        vn=None,
-        sn=None,
-        thread_queue_size=8192,
-    )
-    output = ffmpeg.output(
-        in_modified,
-        in_original,
-        output_path,
-        acodec="copy",
-        crf=11,
-        preset="faster",
-        threads=16,
-        framerate=video_info.framerate,
-        s=f"{video_info.width}x{video_info.height}",
-        # strict="-2", truehd audio causes issues
-    )
-    process = output.overwrite_output().run_async(pipe_stdin=True)
-    for frame in video_buffer:
-        process.stdin.write(frame.tobytes())
-    process.stdin.close()
-    process.wait()
 
 def create_side_by_side_video(video_buffer: NDArray, max_shift: int):
     num_frames, height, width, _ = video_buffer.shape
@@ -94,15 +42,86 @@ def create_side_by_side_video(video_buffer: NDArray, max_shift: int):
 
 
 if __name__ == "__main__":
-    filepath = "./in_rgbd/upsidedown_video_depth_125922.mp4"
-    max_shift = 30
+    os.makedirs('./build/sbs', exist_ok=True)
 
+    filename = "sw_qt_video_depth_211953.mp4"
+    filepath = f"./build/depth/{filename}"
+    max_shift = 20
+
+    output_path = f"./build/sbs/{max_shift}-{filename}"
     video_info = get_video_info(filepath)
-    video_buffer = load_video_buffer(filepath, video_info)
-    side_by_side_buffer = create_side_by_side_video(video_buffer, max_shift)
+    output_width = video_info.width
+
+    CHUNK_SIZE = 32 * 1024 * 1024 * 1024  # GB
+    CHUNK_FRAMES = CHUNK_SIZE // (video_info.width * video_info.height * 3)
+
+    vbuffer = np.zeros((CHUNK_FRAMES, video_info.height, output_width, 3), dtype=np.uint8)
+
+    process_input = (
+        ffmpeg.input(
+            filepath,
+            threads=0,
+            thread_queue_size=8192,
+        )
+        .output("pipe:", format="rawvideo", pix_fmt="rgb24", loglevel="quiet")
+        .run_async(pipe_stdout=True)
+    )
 
 
-    now = datetime.now()
-    hourminsec = now.strftime("%H%M%S")
-    output_path = f"./out/musicvid-sbs-{hourminsec}.mp4"
-    write_output_video(output_path, side_by_side_buffer, video_info, filepath)
+    in_modified = ffmpeg.input(
+        "pipe:",
+        format="rawvideo",
+        pix_fmt="rgb24",
+        s=f"{output_width}x{video_info.height}",
+        framerate=video_info.framerate,
+        thread_queue_size=8192,
+    )
+    in_original = ffmpeg.input(
+        filepath,
+        thread_queue_size=8192,
+        vn=None,
+        sn=None,
+    )
+    process_output = ffmpeg.output(
+        in_modified,
+        in_original,
+        output_path,
+        acodec="copy",
+        crf=11,
+        vcodec="libx264",
+        pix_fmt="yuv420p",
+        preset="faster",
+        threads=0,
+        framerate=video_info.framerate,
+        s=f"{output_width}x{video_info.height}",
+        loglevel="quiet",
+    ).overwrite_output().run_async(pipe_stdin=True)
+
+    progress_bar = tqdm(total=video_info.num_frames, unit="frames")
+
+    for chunk_start in range(0, video_info.num_frames, CHUNK_FRAMES):
+        chunk_end = min(chunk_start + CHUNK_FRAMES, video_info.num_frames)
+        chunk_frames = chunk_end - chunk_start
+
+        progress_bar.set_description('read')
+        for i in range(chunk_frames):
+            in_bytes = process_input.stdout.read(video_info.width * video_info.height * 3)
+            if not in_bytes:
+                break
+
+            vbuffer[i, :, :video_info.width, :] = np.frombuffer(in_bytes, np.uint8).reshape(
+                [video_info.height, video_info.width, 3]
+            )
+
+        progress_bar.set_description('process')
+        vbuffer[:chunk_frames] = create_side_by_side_video(vbuffer[:chunk_frames], max_shift)
+
+        progress_bar.set_description('write')
+        for frame in vbuffer[:chunk_frames]:
+            process_output.stdin.write(frame.tobytes())
+            progress_bar.update(1)
+
+    process_input.wait()
+    process_output.stdin.close()
+    process_output.wait()
+    progress_bar.close()
